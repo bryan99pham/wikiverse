66d0922c56790f81b693909d7720d3a210fbaa3c
bryan99pham <bryan99pham@gmail.com>
2022-09-14 13:29:45 -0500

    started writing article component

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { PagesList } from './PagesList';
+import { Article } from './Article';
 
 // import and prepend the api url to any fetch calls
 import apiURL from '../api';
@@ -7,12 +8,22 @@ import apiURL from '../api';
 export const App = () => {
 
 	const [pages, setPages] = useState([]);
+	const [article, setArticle] = useState(null);
 
 	async function fetchPages(){
 		try {
 			const response = await fetch(`${apiURL}/wiki`);
 			const pagesData = await response.json();
 			setPages(pagesData);
+		} catch (err) {
+			console.log("Oh no an error! ", err)
+		}
+	}
+	async function fetchArticleData(slug){
+		try {
+			const response = await fetch(`${apiURL}/wiki/${slug}`);
+			const articleData = await response.json();
+			setArticle(articleData);
 		} catch (err) {
 			console.log("Oh no an error! ", err)
 		}
@@ -26,7 +37,10 @@ export const App = () => {
 		<main>	
       <h1>WikiVerse</h1>
 			<h2>An interesting 📚</h2>
-			<PagesList pages={pages} />
+			{
+				article ? <Article props={article} handleClick={fetchPages} /> : <PagesList pages={pages} handleClick={fetchArticleData} />
+			}
+			
 		</main>
 	)
 }
--- public/react/components/PagesList.js
+++ public/react/components/PagesList.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Page } from './Page';
 
-export const PagesList = ({pages}) => {
+export const PagesList = ({pages, handleClick}) => {
 	return <>
 		{
 			pages.map((page, idx) => {
-				return <Page page={page} key={idx} />
+				return <Page page={page} key={idx} handleClick={handleClick}/>
 			})
 		}
 	</>
